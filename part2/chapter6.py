8ca1c5147b4b7953befc9572174d6e4189eff04f
Damian Wójcik <macmacroll@gmail.com>
2020-12-08 23:57:12 +0100

    chapter 6 end

---
+++ part2/chapter6.py
@@ -0,0 +1,18 @@
+#zmienne odnoszą się do określonych obiektów zaalokowanych w 
+#pamięci ,są jak wskaźniki na nie ,są tylko nazwami ,
+#dlatego można przypisywać im różne typy
+#Po prostu odnoszą się do konkretnego obiektu w określonym
+#momencie
+a = 3 
+a = 'ABCDS'
+a = 2.23
+#działa 
+#zmienna zawiera referencję do obiektu
+#Obiekty zawierają dwa pola nagłówków - desygnator typu
+#i licznik referencji
+
+#po usunięciu ostatniej referencji do obiektu 
+
+b = 3 +3j
+c = b
+#teraz zmienne odnoszą się do tego samego obiektu
