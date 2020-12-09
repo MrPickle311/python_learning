73e3df1a685364026296a7eaf55b59c91daf8015
Damian Wójcik <macmacroll@gmail.com>
2020-12-09 13:50:44 +0100

    01.12.2020

---
+++ part2/chapter7.py
@@ -0,0 +1,14 @@
+#łańcuchy są niemutowalne
+s1 = " 'hello' " #tak możemy dodać sobie cudzysłowy
+s2 = 'hello'
+print(s1)
+print(s2)
+s3 = '"hello"'
+print(s3)
+#apostrofy i cudzysłowy są stosowane wymiennie 
+#są po to ,by móc się w sobie nawzajem osadzać
+
+e = "Hello " 'World' # niejawna konkatencja 
+print(e)
+
+print('\' hello \' ') #jednak tak też można umieścić apostrofy
