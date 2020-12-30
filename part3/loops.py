dd3b4f1297650c2d5a1d9d628769997902c38a17
Damian Wójcik <macmacroll@gmail.com>
2020-12-30 17:15:23 +0100

    30/12/2020

--- part3/loops.py
+++ part3/loops.py
@@ -51,7 +51,73 @@ while False: # jeśli pętla nie zostanie ani razu wykonana ,to i tak wchodzi
 else:
     print("False")
 
+L = [1,2,3,4,5]
+while len(L) != 0:
+    print(L)
+    L.pop()
+else:
+    L = [99,99,99]
+print(L)
 
-# ilość godzin + 1
+while L:
+    if(len(L) == 1):
+        break
+    L.pop()
+else:
+    print("Im in the else !")
+#jak widać break nie pozwala na przejście do else
 
-#443 Więcej o części pętli else
+#while ((x = next(obj)) != NULL) {...przetwarzanie x...}
+#w Pythone nie można tak zagnieżdżać wyrażeń , wszystko musi być policzone
+#na zewnątrz
+
+#pętle for
+
+#obsługa krotek i słowników
+L = [(4,4),(2,2),(1,5),(9,0)]
+for (x,y) in L:
+    print(x,y)
+
+D = dict(rommel="abc",wow=34,crt='sad')
+
+for node in D.items():
+    key,val = node
+    print(key,val)
+
+for key in D.keys():
+    print(key)
+
+L = [((1,2),3),((1,2),3),((1,2),3)]
+
+#obsługa zagnieżdżeń
+for ((x,y),z) in L:
+    print(x,y,z)
+
+L = [[1,2],[1,2,3],[1,2,3,4]]
+for a,*b,c in L:
+    print(a)
+    for x in b: print(x) 
+    print(c)
+
+#else
+
+items = [13,"232d","maya",(6,5)]
+keys = ["robert","maya","esteban"]
+
+for key in keys:
+    for item in items:
+        if key == item:
+            print("Dopasowano!")
+            break
+    else: #Wykonywane po ostatniej iteracji pętli 
+        print("Nie znaleziono")
+
+#uwaga zmienne przed in np x in są kopiami ,ale tylko w przypadku typów ,wiec...
+#niemutowalnych
+
+L = [1,2,3,4,5]
+for x in L:
+    x = 0
+print(L)
+
+#Warto pamiętać — skanery plików 450
