bf8f73479ae8c9be2bbd60fd90410793bc67ec20
MrPickle311 <macmacroll@gmail.com>
2021-08-23 19:41:03 +0200

    1035

--- part6/advanced.py
+++ part6/advanced.py
@@ -79,3 +79,76 @@ if __name__ == '__main__':
 # Klasy w nowym stylu
 
 # 1011
+
+# POMINĄŁEM : Pomijanie instancji we wbudowanych operacjach
+# przy pobieraniu atrybutów 
+
+# Klasy są typami
+# Obiekt type generuje klasy będące jego instancjami, natomiast klasy generują instancje
+# swoich typów. Oba pojęcia są typami, ponieważ generują instancje
+
+# Wszystkie obiekty dziedziczą po klasie object
+
+class C: pass
+X = C()
+
+print(type(X), type(C))
+print(isinstance(X, object))
+print(isinstance(C, object))
+
+# to samo tyczy się typów wbudowanych
+
+print(isinstance('spam', object))
+print(isinstance(str, object))
+
+# i type
+
+print(isinstance(type, object))
+
+# Zmiany w dziedziczeniu diamentowym
+
+# lewo , a potem do góry
+
+class A: attr = 1
+class B(A): pass
+class C(A): attr = 2
+class D(B,C): pass
+
+x = D()
+print(x.attr) 
+# stary styl : Kolejność przeszukiwania: x, D, B, A
+# nowy styl : Kolejność przeszukiwania: x, D, B, C
+
+# ten mechanizm tzw. MRO można śledzić poprzez __mro__
+
+print(D.__mro__)
+
+print('\n\n')
+# Sloty: deklaracje atrybutów
+
+# Aby móc używać slotów, należy przypisać sekwencję nazw specjalnej zmiennej __slots__
+# i atrybutowi na najwyższym poziomie instrukcji class . Tylko te nazwy, które znajdują się na
+# liście __slots__ , mogą być atrybutami instancji, którym będzie można przypisywać wartości.
+
+class limiter(object):
+    __slots__ = ['age', 'name', 'job']
+
+# Przed użyciem należy przypisać wartość
+
+x.age = 40
+print(x.age)
+
+x.h = 6
+print(x.h)
+
+# slotów nie należy używać z wyjątkiem naprawdę szczególnych przypadków
+# wykorzystujemy je , gdy wykorzystanie pamięci jest kwestią krytyczną
+
+# Gdy tworzonych jest wiele instancji, a wymaganych jest tylko kilka atrybutów, wtedy przydziela-
+# nie słownika przestrzeni nazw każdemu obiektowi instancji może być kosztowną operacją pod
+# względem wykorzystania pamięci. Aby oszczędzić miejsce, Python nie przydziela słownika każ-
+# dej instancji, tylko rezerwuje w każdej z nich przestrzeń niezbędną do przechowywania
+# wartości każdego atrybutu slotu wraz z odziedziczonymi atrybutami we wspólnej klasie na
+# potrzeby zarządzania dostępem do slotów. W ten sposób przyspiesza się wykonywanie kod
+
+# Sloty i słowniki przestrzeni nazw 1035
